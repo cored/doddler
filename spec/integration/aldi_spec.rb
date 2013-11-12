be67c4136bd31e5ec55fab986acb57f4a4a6d99d
Rafael George <george.rafael@gmail.com>
2013-11-12 12:27:20 -0400

    Add failing spec for find method

--- spec/integration/aldi_spec.rb
+++ spec/integration/aldi_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe DeAldiFernbus,".find" do 
+
+  it 'return an array of segments hashes' do 
+    from = "Berlin FH T"
+    to = "Dresden NST"
+    departure = DateTime.civil(2013,12,1)
+    response = DeAldiFernbus.find(from, to, departure)
+
+    expect(response.size).to eql(1)
+  end
   
 end
